0069e2aeb7cd3781598a9f0d842e5d3c3b4c5672
jihoon <git40@gmail.com>
2026-03-15 20:32:56 +0900

    상품 폴더 설계

---
+++ src/main/java/org/example/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,8 @@
+package org.example.myselectshop.repository;
+
+import org.example.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+}
---
+++ src/main/java/org/example/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,8 @@
+package org.example.myselectshop.repository;
+
+import org.example.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+
+}
